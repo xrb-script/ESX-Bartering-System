e57ce517bce266a5247abe3629181e30f92b4a4d
xrb-script <truexrb@gmail.com>
2025-05-19 11:37:12 +0200

    Update al.lua

--- locales/al.lua
+++ locales/al.lua
@@ -10,3 +10,7 @@
 -- Lang['al']['some_new_message_key'] = "Ky eshte nje mesazh i ri."
 
 -- You could still copy Config.Messages here for reference if desired, but it's not directly used by the script as converted.
+
+-- xResul Albania
+
+-- Discord : http://discord.gg/CAyUh9su2s
